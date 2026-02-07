99995076e24afaac138013dc18f93b371eff84d9
Anna Lin <annalin1510@outlook.com>
2026-02-07 23:21:08 +0000

    Add variables to track current and previous cards

--- src/main/Snap.java
+++ src/main/Snap.java
@@ -2,12 +2,14 @@ package main;
 import java.util.Scanner;
 
 public class Snap extends CardGame {
-
+    private Card previousCard;
+    private Card currentCard;
+    Scanner scanner = new Scanner(System.in);
+    
     public Snap() {
         super("Snap");
     }
 
-    Scanner scanner = new Scanner(System.in);
 
 
 
