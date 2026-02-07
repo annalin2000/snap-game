17b2e347e6191704e9d20b8118c9f330e9b99c60
Anna Lin <annalin1510@outlook.com>
2026-02-07 23:22:13 +0000

    Add play method to Snap game

--- src/main/Snap.java
+++ src/main/Snap.java
@@ -5,12 +5,15 @@ public class Snap extends CardGame {
     private Card previousCard;
     private Card currentCard;
     Scanner scanner = new Scanner(System.in);
-    
+
     public Snap() {
         super("Snap");
     }
 
-
+    public void play() {
+        shuffleDeck();
+        System.out.println("Press ENTER to deal a card.");
+    }
 
 
 
