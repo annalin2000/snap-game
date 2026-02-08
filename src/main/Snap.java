e97593e04fa18fd5ac60f09d6f2b753b8e27cbc8
Anna Lin <annalin1510@outlook.com>
2026-02-08 13:53:36 +0000

    Initialise p1 and p2 via Snap constructor

--- src/main/Snap.java
+++ src/main/Snap.java
@@ -8,8 +8,10 @@ public class Snap extends CardGame {
     private Card currentCard;
     Scanner scanner = new Scanner(System.in);
 
-    public Snap() {
+    public Snap(Player p1, Player p2) {
         super("Snap");
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
     public void play() {
