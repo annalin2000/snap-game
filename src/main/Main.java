50a95fd901575037e26dad99b081045708620214
Anna Lin <annalin1510@outlook.com>
2026-02-08 10:58:58 +0000

    Read player names and create Player objects

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,17 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Player 1 name: ");
+        Player p1 = new Player(scanner.nextLine());
+
+        System.out.print("Enter Player 2 name: ");
+        Player p2 = new Player(scanner.nextLine());
+
         Snap snap = new Snap();
         snap.play();
     }
